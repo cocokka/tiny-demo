673679742a5d75b6ff9ebcea6b4d42986adb3efb
LGP <651011260@qq.com>
2024-03-02 22:45:05 +0800

    oauth2 login

---
+++ demo-oauth2-login/src/main/java/com/tinydemo/demooauth2login/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.tinydemo.demooauth2login.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+			@AuthenticationPrincipal OAuth2User oAuth2User) {
+
+		model.addAttribute("username", oAuth2User.getName());
+		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+		model.addAttribute("userAttributes", oAuth2User.getAttributes());
+
+		return "index";
+	}
+
+}
